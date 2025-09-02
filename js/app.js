abcd80aee586ca2b4a79183a86141d8d379fd288
Carlos Sousa <carlossousa23@icloud.com>
2025-09-01 23:32:38 -0300

    adicionando um novo slide no 'swiper' com outro tema, e fazendo funcionar corretamente

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.swiper', {
         direction: 'horizontal',
-        loop: true,
         navigation: {
-            nextEl:'.swiper-button-next',
-            prevEl:'.swiper-button-prev'
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
         }
     })
 })
+
+
